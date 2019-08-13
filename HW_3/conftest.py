a50c875e3bef17fb4e74f2c0e6f138c3f44dfd7a
XFanis <mail@xfanis.ru>
2019-08-13 11:09:04 +0300

    HW-3-1: dog.ceo api tests

--- HW_3/conftest.py
+++ HW_3/conftest.py
@@ -1,18 +1,18 @@
-import pytest
-
-from HW_3.api_client import APIClient
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--url",
-        action="store",
-        default="https://dog.ceo/",
-        help="This is request url"
-    )
-
-
-@pytest.fixture(scope="session")
-def api_client(request):
-    base_url = request.config.getoption("--url")
-    return APIClient(base_address=base_url)
+# import pytest
+#
+# from HW_3.api_client import APIClient
+#
+#
+# def pytest_addoption(parser):
+#     parser.addoption(
+#         "--url",
+#         action="store",
+#         default="https://dog.ceo/",
+#         help="This is request url"
+#     )
+#
+#
+# @pytest.fixture(scope="session")
+# def api_client(request):
+#     base_url = request.config.getoption("--url")
+#     return APIClient(base_address=base_url)
