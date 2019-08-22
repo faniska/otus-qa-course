2c20da49b7969efa0636806ba6b8dfa862c8b47f
XFanis <ya.faniska@gmail.com>
2019-08-22 19:19:39 +0300

    HW_5: Refactoring
    Use browser fixture as separate module to be able re-use for other HW's

--- HW_5/test_opencart.py
+++ HW_5/test_opencart.py
@@ -1,52 +1,22 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver import ChromeOptions, FirefoxOptions
+from assets.fixtures.Browser import Browser
 
 
-@pytest.fixture(scope='module')
-def browser(request):
-    browser = request.config.getoption("--browser")
-    url = request.config.getoption("--url")
+class TestOpencart(Browser):
 
-    if browser.lower() == 'chrome':
-        options = ChromeOptions()
-        options.add_argument("--headless")
-        wd = webdriver.Chrome(options=options)
-    elif browser.lower() == 'firefox':
-        options = FirefoxOptions()
-        options.add_argument("--headless")
-        wd = webdriver.Firefox(options=options)
-    elif browser.lower() == 'safari':
-        wd = webdriver.Safari()
-    else:
-        raise ValueError('--browser option can have chrome or firefox value')
-    request.addfinalizer(wd.quit)
+    def test_opencart_page(self, wd):
+        """
+        Check if main page contains correct title
+        """
+        assert wd.title == 'Your Store'
 
-    wd.get(url)
-    return wd
+    def test_search_field(self, wd):
+        """
+        Check if page contains search field
+        """
+        assert wd.find_element_by_id('search')
 
-
-@pytest.fixture
-def get_url(request):
-    return request.config.getoption("--url")
-
-
-def test_opencart_page(browser):
-    """
-    Check if main page contains correct title
-    """
-    assert browser.title == 'Your Store'
-
-
-def test_search_field(browser):
-    """
-    Check if page contains search field
-    """
-    assert browser.find_element_by_id('search')
-
-
-def test_shopping_cart_link(browser):
-    """
-    Check if page contains shopping cart link
-    """
-    assert browser.find_elements_by_css_selector('a[title="Shopping Cart"]')
+    def test_shopping_cart_link(self, wd):
+        """
+        Check if page contains shopping cart link
+        """
+        assert wd.find_elements_by_css_selector('a[title="Shopping Cart"]')
---
+++ assets/fixtures/Browser.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions, FirefoxOptions
+
+
+class Browser:
+
+    @pytest.fixture
+    def wd(self, request):
+        browser = request.config.getoption("--browser")
+        url = request.config.getoption("--url")
+
+        if browser.lower() == 'chrome':
+            options = ChromeOptions()
+            options.add_argument("--headless")
+            wd = webdriver.Chrome(options=options)
+        elif browser.lower() == 'firefox':
+            options = FirefoxOptions()
+            options.add_argument("--headless")
+            wd = webdriver.Firefox(options=options)
+        elif browser.lower() == 'safari':
+            wd = webdriver.Safari()
+        else:
+            raise ValueError('--browser option can have chrome or firefox value')
+        request.addfinalizer(wd.quit)
+
+        wd.get(url)
+        return wd
