babcaabf2713bc27f4091e1b771f29469f128b25
XFanis <ya.faniska@gmail.com>
2019-09-05 12:10:59 +0300

    HW_10: Refactoring HW_9 test using page object

--- HW_9/test_waiting.py
+++ HW_9/test_waiting.py
@@ -1,43 +1,16 @@
-import pytest
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-
 from assets.fixtures.Browser import Browser
-from assets.locators import AdminLogin, AdminPage
+from assets.locators import AdminPage
 from assets.opencart import Credentials
+
+from assets.page_objects import AdminPO
 
 
 class TestProductPageWaiting(Browser, Credentials):
     headless = False
 
-    @pytest.fixture()
-    def admin_page_wd(self, wd):
+    def test_product_page(self, wd):
+        AdminPO(wd).login() \
+            .hide_security_alert() \
+            .open_catalog()
 
-        wd.find_element_by_css_selector(AdminLogin.input_username['css']).send_keys(self.login)
-        wd.find_element_by_css_selector(AdminLogin.input_password['css']).send_keys(self.pwd)
-
-        wd.find_element_by_css_selector(AdminLogin.button['css']).click()
-        # Waiting for the security alert block to close it
-        wait = WebDriverWait(wd, 2)
-        button = wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, AdminPage.button_close['css'])))
-        button.click()
-        return wd
-
-    def test_product_page(self, admin_page_wd):
-        # Click to Catalog
-        admin_page_wd.find_element_by_css_selector(AdminPage.catalog_link['css']).click()
-        # Waiting 2 sec(s) for expanding the catalog menu and try to find link by text
-        try:
-            wait = WebDriverWait(admin_page_wd, 2)
-            products_link = wait.until(
-                expected_conditions.presence_of_element_located((By.PARTIAL_LINK_TEXT, AdminPage.products_link['text']))
-            )
-            products_link.click()
-            assert admin_page_wd.find_element_by_css_selector(AdminPage.form_product['css'])
-
-        except NoSuchElementException:
-            assert False, 'Please fix the products link text!'
-        except TimeoutException:
-            assert False, 'Timeout is raised! Please correct locator or increase waiting time'
+        assert AdminPO(wd).open_products() and wd.find_element_by_css_selector(AdminPage.form_product['css'])
--- assets/page_objects/AdminPO.py
+++ assets/page_objects/AdminPO.py
@@ -1,5 +1,41 @@
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+from assets.locators import AdminLogin, AdminPage
 from .BasePO import BasePO
+from assets.opencart import Credentials
 
 
 class AdminPO(BasePO):
-    pass
+    def login(self):
+        self._input(AdminLogin.input_username, Credentials.login)
+        self._input(AdminLogin.input_password, Credentials.pwd)
+        self._click(AdminLogin.button)
+        return self
+
+    def hide_security_alert(self):
+        wait = WebDriverWait(self.wd, 2)
+        locator = (By.CSS_SELECTOR, AdminPage.button_close['css'])
+        button = wait.until(expected_conditions.presence_of_element_located(locator))
+        button.click()
+        return self
+
+    def open_catalog(self):
+        self._click(AdminPage.catalog_link)
+        return self
+
+    def open_products(self):
+        try:
+            wait = WebDriverWait(self.wd, 2)
+            products_link = wait.until(
+                expected_conditions.presence_of_element_located((By.PARTIAL_LINK_TEXT, AdminPage.products_link['text']))
+            )
+            products_link.click()
+            return True
+
+        except NoSuchElementException:
+            assert False, 'Please fix the products link text!'
+        except TimeoutException:
+            assert False, 'Timeout is raised! Please correct locator or increase waiting time'
