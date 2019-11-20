75c7ea5d8fbabc4877d6d3be53f4dbf381898451
XFanis <ya.faniska@gmail.com>
2019-11-20 19:36:28 +0300

    HW_23: Script to show sys info (2)

--- HW_23/show_sys_info.py
+++ HW_23/show_sys_info.py
@@ -1,4 +1,14 @@
+import os
 import subprocess
+
+
+def show_package_version(package):
+    subprocess.check_call(f'pip3 freeze | grep {package}', shell=True)
+
+
+def show_dir_content(path):
+    subprocess.check_call(f'ls {path}', shell=True)
+
 
 print('Network Interfaces')
 subprocess.check_call(['netstat', '-i'])
@@ -23,4 +33,38 @@ print('----------')
 
 print('All Processes')
 subprocess.check_call(['ps', 'aux'])
-print('--   --------')
+print('----------')
+
+print('Show apache2 status')
+try:
+    subprocess.check_call(['service', 'apache2', 'status'])
+except FileNotFoundError as e:
+    print('Command not found')
+print('----------')
+
+print('Port 6942 status')
+subprocess.check_call('lsof -i :6942', shell=True)
+print('----------')
+
+print('Show python package version')
+show_package_version('requests')
+print('----------')
+
+print('Show dir content')
+show_dir_content('/')
+print('----------')
+
+print('Show current directory')
+subprocess.check_call(['pwd'])
+print('----------')
+
+print('Show kernel version')
+subprocess.check_call('uname -r', shell=True)
+print('----------')
+
+print('Show operating system version')
+
+if os.uname()[0] == 'Darwin':
+    subprocess.check_call('sw_vers', shell=True)
+elif os.uname()[0] == 'Linux':
+    subprocess.check_call('lsb_release -d', shell=True)
